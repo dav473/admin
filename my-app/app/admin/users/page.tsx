8ea4e43631415c77ced29348baa6672f580c4ede
zhen <zhen.zhang@conversance.co>
2024-02-06 16:53:43 -0500

    working on breadcrumb

--- my-app/app/admin/users/page.tsx
+++ my-app/app/admin/users/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import BreadCrumb from "@/components/breadcrumb";
 
-const UsersPage = () => {
+import { users } from "@/constants/data";
+
+const breadcrumbItems = [{ title: "User", link: "/dashboard/user" }];
+export default function page() {
   return (
-    <div>UsersPage</div>
-  )
+    <>
+      <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
+        <BreadCrumb items={breadcrumbItems} />
+      </div>
+    </>
+  );
 }
-
-export default UsersPage
